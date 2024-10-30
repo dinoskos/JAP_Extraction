c292e264b020bb274f66af98404bf1d80a938b8d
dinoskos <dinoskos.datascience@gmail.com>
2024-10-30 09:48:23 -0400

    Add setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MetaData_Extraction',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # List your dependencies here
+    description='A package for extracting information from articles DOI',
+    author='Bao Ho',
+    author_email='bh.baoho@gmail.com',
+    url='https://github.com/dinoskos/MetaData_Extraction',
+)
